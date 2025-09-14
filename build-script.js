5884eaa1270ebc63bc1d7852816ccbd787245dc2
Tihh Gonçalves <tihhgoncalves@gmail.com>
2025-09-14 11:12:14 -0300

    🔧 Atualiza o fluxo de trabalho de deploy para sincronizar apenas arquivos específicos e corrige o diretório do servidor

--- build-script.js
+++ build-script.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const packageJson = require('./package.json');
+const { minify } = require('terser');
 
 const distPath = path.join(__dirname, 'public');
 
@@ -13,3 +14,9 @@ const source = fs.readFileSync('./src/whatsfloat.js', 'utf-8');
 const output = source.replace('%VERSION%', version);
 
 fs.writeFileSync('./public/whatsfloat.js', output);
+
+// Gerar versão minificada
+(async () => {
+  const minified = await minify(output, { format: { comments: false } });
+  fs.writeFileSync('./public/whatsfloat.min.js', minified.code);
+})();
